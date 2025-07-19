cd75e4d086de3e26b5a4d52e3cee2fd815eb0401
Jorge Ramos <jorgeramos@gmail.com>
2025-07-19 19:24:10 -0400

    Create Merchant Order Workflow

---
+++ backend/src/workflows/marketplace/create-merchant-orders/index.ts
@@ -0,0 +1,69 @@
+import { createWorkflow, WorkflowResponse } from "@medusajs/framework/workflows-sdk";
+import {
+  completeCartWorkflow,
+  createRemoteLinkStep,
+  getOrderDetailWorkflow,
+  useQueryGraphStep,
+} from "@medusajs/medusa/core-flows";
+import createMerchantOrdersStep from "./steps/create-merchant-orders";
+import groupMerchantItemsStep from "./steps/group-merchant-items";
+
+export type CreateMerchantOrderWorkflowInput = {
+  cart_id: string;
+};
+
+const createMerchantOrdersWorkflow: any = createWorkflow(
+  "create-merchant-order",
+  (input: CreateMerchantOrderWorkflowInput) => {
+    const { data: carts } = useQueryGraphStep({
+      entity: "cart",
+      fields: ["id", "items.*"],
+      filters: { id: input.cart_id },
+      options: {
+        throwIfKeyNotFound: true,
+      },
+    });
+
+    const { id: orderId } = completeCartWorkflow.runAsStep({
+      input: {
+        id: carts[0].id,
+      },
+    });
+
+    const { merchantsItems } = groupMerchantItemsStep({
+      cart: carts[0],
+    });
+
+    const order = getOrderDetailWorkflow.runAsStep({
+      input: {
+        order_id: orderId,
+        fields: [
+          "region_id",
+          "customer_id",
+          "sales_channel_id",
+          "email",
+          "currency_code",
+          "shipping_address.*",
+          "billing_address.*",
+          "shipping_methods.*",
+          "shipping_methods.tax_lines.*",
+          "shipping_methods.adjustments.*",
+        ],
+      },
+    });
+
+    const { orders: merchantOrders, linkDefs } = createMerchantOrdersStep({
+      parentOrder: order,
+      merchantsItems,
+    });
+
+    createRemoteLinkStep(linkDefs);
+
+    return new WorkflowResponse({
+      parent_order: order,
+      merchant_orders: merchantOrders,
+    });
+  }
+);
+
+export default createMerchantOrdersWorkflow;
---
+++ backend/src/workflows/marketplace/create-merchant-orders/steps/create-merchant-orders.ts
@@ -0,0 +1,127 @@
+import { CartLineItemDTO, InferTypeOf, LinkDefinition, OrderDTO } from "@medusajs/framework/types";
+import { Modules, promiseAll } from "@medusajs/framework/utils";
+import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk";
+import { cancelOrderWorkflow, createOrderWorkflow } from "@medusajs/medusa/core-flows";
+import { MARKETPLACE_MODULE } from "../../../../modules/marketplace";
+import Merchant from "../../../../modules/marketplace/models/merchant";
+import MarketplaceModuleService from "../../../../modules/marketplace/service";
+
+export type MerchantOrder = OrderDTO & {
+  merchant: InferTypeOf<typeof Merchant>;
+};
+
+type StepInput = {
+  parentOrder: OrderDTO;
+  merchantsItems: Record<string, CartLineItemDTO[]>;
+};
+
+function prepareOrderData(items: CartLineItemDTO[], parentOrder: OrderDTO) {
+  return {
+    items,
+    metadata: {
+      parent_order_id: parentOrder.id,
+    },
+    // use info from parent
+    region_id: parentOrder.region_id,
+    customer_id: parentOrder.customer_id,
+    sales_channel_id: parentOrder.sales_channel_id,
+    email: parentOrder.email,
+    currency_code: parentOrder.currency_code,
+    shipping_address_id: parentOrder.shipping_address?.id,
+    billing_address_id: parentOrder.billing_address?.id,
+    // A better solution would be to have shipping methods for each
+    // item/merchant. This requires changes in the storefront to commodate that
+    // and passing the item/merchant ID in the `data` property, for example.
+    // For simplicity here we just use the same shipping method.
+    shipping_methods: parentOrder.shipping_methods.map((shippingMethod) => ({
+      name: shippingMethod.name,
+      amount: shippingMethod.amount,
+      shipping_option_id: shippingMethod.shipping_option_id,
+      data: shippingMethod.data,
+      tax_lines: shippingMethod.tax_lines.map((taxLine) => ({
+        code: taxLine.code,
+        rate: taxLine.rate,
+        provider_id: taxLine.provider_id,
+        tax_rate_id: taxLine.tax_rate_id,
+        description: taxLine.description,
+      })),
+      adjustments: shippingMethod.adjustments.map((adjustment) => ({
+        code: adjustment.code,
+        amount: adjustment.amount,
+        description: adjustment.description,
+        promotion_id: adjustment.promotion_id,
+        provider_id: adjustment.provider_id,
+      })),
+    })),
+  };
+}
+
+const createMerchantOrdersStep = createStep(
+  "create-merchant-orders",
+  async ({ merchantsItems, parentOrder }: StepInput, { container, context }) => {
+    const linkDefs: LinkDefinition[] = [];
+    const createdOrders: MerchantOrder[] = [];
+    const merchantIds = Object.keys(merchantsItems);
+
+    const marketplaceModuleService: MarketplaceModuleService = container.resolve(MARKETPLACE_MODULE);
+
+    const merchants = await marketplaceModuleService.listMerchants({
+      id: merchantIds,
+    });
+
+    try {
+      await promiseAll(
+        merchantIds.map(async (merchantId) => {
+          const items = merchantsItems[merchantId];
+          const merchant = merchants.find((v) => v.id === merchantId)!;
+
+          const { result: childOrder } = (await createOrderWorkflow(container).run({
+            input: prepareOrderData(items, parentOrder),
+            context,
+          })) as unknown as { result: MerchantOrder };
+
+          childOrder.merchant = merchant;
+          createdOrders.push(childOrder);
+
+          linkDefs.push({
+            [MARKETPLACE_MODULE]: {
+              merchant_id: merchant.id,
+            },
+            [Modules.ORDER]: {
+              order_id: childOrder.id,
+            },
+          });
+        })
+      );
+    } catch (e) {
+      return StepResponse.permanentFailure(`An error occured while creating merchant orders: ${e}`, {
+        created_orders: createdOrders,
+      });
+    }
+
+    return new StepResponse(
+      {
+        orders: createdOrders,
+        linkDefs,
+      },
+      {
+        created_orders: createdOrders,
+      }
+    );
+  },
+  async ({ created_orders }, { container, context }) => {
+    await Promise.all(
+      created_orders.map((createdOrder) => {
+        return cancelOrderWorkflow(container).run({
+          input: {
+            order_id: createdOrder.id,
+          },
+          context,
+          container,
+        });
+      })
+    );
+  }
+);
+
+export default createMerchantOrdersStep;
