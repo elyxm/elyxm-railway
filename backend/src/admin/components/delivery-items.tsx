a4ecd17ebe13af4eec7a791fdb273e2c3f35ac2a
Jorge Ramos <jorgeramos@gmail.com>
2025-07-29 15:04:23 -0400

    feat: Add delivery and driver management components, including actions menus and delivery item displays, to enhance admin interface

---
+++ backend/src/admin/components/delivery-items.tsx
@@ -0,0 +1,58 @@
+import { Container, Heading, Text } from "@medusajs/ui";
+import { useState } from "react";
+import { DeliveryDTO } from "../../modules";
+
+const DeliveryItems = ({ delivery }: { delivery: DeliveryDTO }) => {
+  const [hovered, setHovered] = useState(false);
+
+  // on hovering over the items count, set hovered to true
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  // on leaving the items count, set hovered to false
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
+  return (
+    <>
+      <div
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className="cursor-context-menu w-full h-full pr-6 py-1 flex items-center"
+      >
+        <span className="border-b border-ui-fg-muted border-dotted font-medium min-w-2">{delivery.items.length}</span>
+      </div>
+      {hovered && (
+        <Container className="absolute flex flex-col gap-2 z-50 right-0 w-fit mr-6">
+          {delivery.items.map((item) => (
+            <DeliveryItem key={item.id} item={item} />
+          ))}
+        </Container>
+      )}
+    </>
+  );
+};
+
+const DeliveryItem = ({ item }: { item: Record<string, any> }) => {
+  const thumbnail = item.thumbnail;
+
+  return (
+    <div className="flex items-center gap-4">
+      <img src={thumbnail} alt={item.title} className="w-16 h-16 rounded-md" width={64} height={64} />
+      <div className="flex justify-between w-full gap-2 items-center">
+        <div className="flex flex-col gap-2base">
+          <Heading level="h3" className="text-sm text-ui-fg-subtle">
+            {item.title}
+          </Heading>
+          <Text className="text-sm text-ui-fg-subtle">
+            {item.quantity} x ${item.unit_price}
+          </Text>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeliveryItems;
