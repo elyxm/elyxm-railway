c38e0491eb536c26d358203d3ff5d63be4a70551
Jorge Ramos <jorgeramos@gmail.com>
2025-07-29 10:05:38 -0400

    feat: Add set step success and set step failed steps to delivery workflow for enhanced error handling

---
+++ backend/src/workflows/delivery/steps/set-step-failed.ts
@@ -0,0 +1,36 @@
+import { ModuleRegistrationName, TransactionHandlerType } from "@medusajs/framework/utils";
+import { StepResponse, createStep } from "@medusajs/workflows-sdk";
+import { DeliveryDTO } from "../../../modules";
+
+type SetStepFailedtepInput = {
+  stepId?: string;
+  updatedDelivery: DeliveryDTO;
+};
+
+export const setStepFailedStepId = "set-step-failed-step";
+
+const setStepFailedStep = createStep(
+  setStepFailedStepId,
+  async function ({ stepId, updatedDelivery }: SetStepFailedtepInput, { container }) {
+    if (!stepId) {
+      return;
+    }
+
+    const engineService = container.resolve(ModuleRegistrationName.WORKFLOW_ENGINE);
+
+    await engineService.setStepFailure({
+      idempotencyKey: {
+        action: TransactionHandlerType.INVOKE,
+        transactionId: updatedDelivery.transaction_id,
+        stepId,
+        workflowId: "handle-delivery-workflow",
+      },
+      stepResponse: new StepResponse(updatedDelivery, updatedDelivery.id),
+      options: {
+        container,
+      },
+    });
+  }
+);
+
+export default setStepFailedStep;
