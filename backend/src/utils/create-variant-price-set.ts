03b6e165b72534e4df3614180c90905d1c9783db
Jorge Ramos <jorgeramos@gmail.com>
2025-07-29 11:33:21 -0400

    refactor: Update import paths to use framework types and streamline code structure across various modules

---
+++ backend/src/utils/create-variant-price-set.ts
@@ -0,0 +1,36 @@
+import { MedusaContainer } from "@medusajs/framework";
+import { CreatePriceSetDTO, IPricingModuleService, PriceSetDTO } from "@medusajs/framework/types";
+import { Modules, remoteQueryObjectFromString } from "@medusajs/framework/utils";
+
+export const createVariantPriceSet = async ({
+  container,
+  variantId,
+  prices,
+}: {
+  container: MedusaContainer;
+  variantId: string;
+  prices: CreatePriceSetDTO["prices"];
+}): Promise<PriceSetDTO> => {
+  const remoteLink = container.resolve("remoteLink");
+  const remoteQuery = container.resolve("remoteQuery");
+  const pricingModuleService: IPricingModuleService = container.resolve("pricingModuleService");
+
+  const priceSet = await pricingModuleService.createPriceSets({
+    prices,
+  });
+
+  await remoteLink.create({
+    [Modules.PRODUCT]: { variant_id: variantId },
+    [Modules.PRICING]: { price_set_id: priceSet.id },
+  });
+
+  const pricingQuery = remoteQueryObjectFromString({
+    entryPoint: "price",
+    fields: ["*"],
+    filters: {
+      id: priceSet.id,
+    },
+  });
+
+  return await remoteQuery(pricingQuery);
+};
